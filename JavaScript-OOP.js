2cec537bc5e1416e765e51cb643ef63b5e04ced3
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-14 23:24:06 +0800

    Paradigm organizing code into objects containing state (properties) and behavior (methods)

---
+++ JavaScript-OOP.js
@@ -0,0 +1,12 @@
+// 1. OOP Fundamentals
+// Topic 1.1: What is OOP?
+// OOP: Paradigm organizing code into objects containing state (properties) and behavior (methods)
+const car = {
+  make: "Toyota",
+  start() {
+    return "Engine started";
+  },
+};
+
+console.log(car.make); // Output: "Toyota"
+console.log(car.start()); // Output: "Engine started"
